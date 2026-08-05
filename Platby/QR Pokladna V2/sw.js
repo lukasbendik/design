bc2a5a03d24f7f71adbdbb853ac8613296f8bc0b
Lukas Bendik <lukinab@gmail.com>
2026-08-05 18:26:16 +0200

    style(qr-pokladna-v2): presunout prepinac meny CZK/EUR pod nadpis K zaplaceni

--- Platby/QR Pokladna V2/sw.js
+++ Platby/QR Pokladna V2/sw.js
@@ -1,5 +1,5 @@
-var CACHE='qr-pokladna-v2-47';
-var ASSETS=['./','./index.html','./styles.css?v=42','./app.js?v=32','./manifest.webmanifest?v=1','../../design-system/tokens.css?v=1','./icon.svg?v=1'];
+var CACHE='qr-pokladna-v2-48';
+var ASSETS=['./','./index.html','./styles.css?v=43','./app.js?v=32','./manifest.webmanifest?v=1','../../design-system/tokens.css?v=1','./icon.svg?v=1'];
 self.addEventListener('install',function(event){self.skipWaiting();event.waitUntil(caches.open(CACHE).then(function(cache){return cache.addAll(ASSETS).catch(function(){});}));});
 self.addEventListener('activate',function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key!==CACHE;}).map(function(key){return caches.delete(key);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(event){
